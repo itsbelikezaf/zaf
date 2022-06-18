875422033c7bc694ae87360a0d279b0fd7159f81
itsbelikezaf <billys1908.com>
2022-06-18 11:19:24 -0700

    ada

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -1,19 +1,29 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  /** On start*/
+  initialize() {
+    this.resetScrollWithoutThreshold(messages);
+  }
+  /** On start */
   connect() {
-    console.log("Connected");
+    console.log("Connected scroll");
     const messages = document.getElementById("messages");
     messages.addEventListener("DOMNodeInserted", this.resetScroll);
-    this.resetScroll(messages);
   }
-  /** On stop*/
+  /** On stop */
   disconnect() {
     console.log("Disconnected");
   }
-  /** Custom function*/
+  /** Custom function */
   resetScroll() {
+    const bottomOfScroll = messages.scrollHeight - messages.clientHeight;
+    const upperScrollThreshold = bottomOfScroll - 500;
+    // Scroll down if we're not within the threshold
+    if (messages.scrollTop > upperScrollThreshold) {
+      messages.scrollTop = messages.scrollHeight - messages.clientHeight;
+    }
+  }
+  resetScrollWithoutThreshold(messages) {
     messages.scrollTop = messages.scrollHeight - messages.clientHeight;
   }
-  }
+}
